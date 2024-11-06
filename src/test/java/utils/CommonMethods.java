8211f1277e79f9322418347aaf977f0dcdf192f5
Lady-Bugz <judithelemo@gmail.com>
2024-11-06 16:09:06 +0100

    Initial commit

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,38 @@
+package utils;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+
+    public static void openBrowserAndLaunchApplication() {
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+
+        switch (ConfigReader.getPropertyValue("browser")){
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+
+
+            default:
+                throw new RuntimeException("Invalid browser name");
+
+        }
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+}
+
